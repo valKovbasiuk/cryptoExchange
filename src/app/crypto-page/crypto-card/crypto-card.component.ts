fd3bba5a9729aafec0b34967abb36f28e9695446
Valentin Kovbasiuk <valentin.mail@gmail.com>
2018-11-29 16:46:29 +0200

    functional toggle

--- src/app/crypto-page/crypto-card/crypto-card.component.ts
+++ src/app/crypto-page/crypto-card/crypto-card.component.ts
@@ -9,10 +9,14 @@ import { CurrencyIndex } from '@angular/common/src/i18n/locale_data';
 export class CryptoCardComponent implements OnInit {
   public percentage: string = '';
   public currencySign: string;
+  public priceOrPercent: string = 'price';
 
   percentToggle(event) {
     console.log(event.target.checked);
-    event.target.checked ? (this.percentage = '%') : (this.percentage = '');
+    event.target.checked
+      ? ((this.percentage = '%'), (this.priceOrPercent = 'percent'))
+      : ((this.percentage = ''), (this.priceOrPercent = 'price'));
+    console.log(this.priceOrPercent);
   }
 
   @Input() currency: string;
